8199574c17a0b8cfaa81c03d391439650b8861a6
ktoshirouq-create <ktoshirouq@gmail.com>
2026-03-21 11:43:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // RUTHLESS RULE: Every time you update your app.js, index.html, or 
 // styles.css on GitHub, you MUST change this version number (e.g., v2, v3).
 // If you do not change this, your phone will load the old cached version forever.
-const CACHE_NAME = 'sendlog-cache-v5'; 
+const CACHE_NAME = 'sendlog-cache-v6'; 
 
 const urlsToCache = [
   './',
