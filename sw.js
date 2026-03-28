e436ab17ef1120f00b068efa59a4f6e0291dda02
ktoshirouq-create <ktoshirouq@gmail.com>
2026-03-28 10:55:47 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,30 @@
-const CACHE_NAME = 'crag-logger-v17';
-const ASSETS = ['./', './index.html', './app.js', './dashboard.html', './dashboard.js'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE_NAME && caches.delete(k))))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'crag-logger-v20';
+const ASSETS = [
+  './', 
+  './index.html', 
+  './app.js', 
+  './dashboard.html', 
+  './dashboard.js'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => 
+      Promise.all(ks.map(k => {
+        if (k !== CACHE_NAME) return caches.delete(k);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
