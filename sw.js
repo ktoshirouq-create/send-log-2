d5d95d3bdfe093cedcb8955bed96c4fd094980a0
ktoshirouq-create <ktoshirouq@gmail.com>
2026-04-05 22:43:38 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crag-logger-v66'; // BUMPED TO V66 TO FORCE UPDATE
+const CACHE_NAME = 'crag-logger-v68'; // BUMPED TO V66 TO FORCE UPDATE
 const ASSETS = [
   '/',
   '/index.html',
